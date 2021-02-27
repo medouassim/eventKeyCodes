2667634d340a6b00fde057433ed9b3f09b7383f1
medouassim <55862605+medouassim@users.noreply.github.com>
2021-02-27 15:44:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener('keydown', (e) => {
+    document.querySelectorAll('.key').forEach(ke => {ke.style.display = 'flex'});
+    var key = e.key;
+    var keyCode = e.keyCode;
+    var code = e.code;
+    document.querySelector('.key:first-child').innerHTML = `${key} <small>event.key</small>`;
+    document.querySelector('.key:nth-child(2)').innerHTML = `${keyCode} <small>event.keycode</small>`;
+    document.querySelector('.key:nth-child(3)').innerHTML =  `${code} <small>event.code</small>`;
+
+})
