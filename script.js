2755ba711d9dc952ee2a13763af342c44a40c64b
medouassim <55862605+medouassim@users.noreply.github.com>
2021-02-27 18:18:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.addEventListener('keydown', (e) => {
     var key = e.key;
     var keyCode = e.keyCode;
     var code = e.code;
-    document.querySelector('.key:first-child').innerHTML = `${key} <small>event.key</small>`;
+    document.querySelector('.key:first-child').innerHTML = `${key === ' '? 'space' : key} <small>event.key</small>`;
     document.querySelector('.key:nth-child(2)').innerHTML = `${keyCode} <small>event.keycode</small>`;
     document.querySelector('.key:nth-child(3)').innerHTML =  `${code} <small>event.code</small>`;
 
